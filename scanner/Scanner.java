2316a47379a1ca0bb125d155b99d50422e78afa2
Jeb Bishop <jeb@jeb.zone>
2021-02-17 02:42:52 -0500

    More work, more play
    Co-authored-by: Josiah Lansford <jrlansford@cedarville.edu>

--- scanner/Scanner.java
+++ scanner/Scanner.java
@@ -1,5 +1,11 @@
 package scanner;
 
+/**
+ * File: Scanner.java
+ * 
+ * Public interface for CMinusScanner to implement.
+ * Defines basic get and view (no munch) methods.
+ */
 public interface Scanner {
   public Token getNextToken();
   public Token viewNextToken();
